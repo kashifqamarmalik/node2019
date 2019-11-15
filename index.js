a438eaade3a651a30874d0d1c468e0da67eebdfd
kashifqamarmalik <kashifqamar111@gmail.com>
2019-11-15 14:04:39 +0200

    database and SELECT * under /test

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 'use strict';
-
+require('dotenv').config()
 const express = require('express');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+database: process.env.DB_NAME
+
+});
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node Server');
+app.use(express.static('public'))
+
+app.get('/test', (req, res) => {
+    connection.query(
+        'SELECT * FROM test.countries',
+        (err, results, fields) => {
+          console.log(results); // results contains rows returned by server
+          console.log(fields); // fields contains extra meta data about results, if available
+          res.json(results);
+        }
+      );
 }); 
 
 app.get('/demo', (req, res) => {
-    res.send('demo');
+    res.send('request', req);
+    res.send('demo')
 });
 app.listen(3000, () => {
     console.log('server app start?');
 });
+
 

db94476702a0ad796d90555776eb716d7b0a4476
kashifqamarmalik <kashifqamar111@gmail.com>
2019-11-21 12:54:01 +0200

    adding model files and test.html

--- index.js
+++ index.js
@@ -1,62 +1,50 @@
-'use strict'
-
+'use strict';
 
 const express = require('express');
-const connection = require('./model/db.js');
-
+const animal = require('./model/animal');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
 app.get('/animals', async (req, res) => {
-
-    // simple query
-    try{
-        const [results, fields] = await connection.query(
-            'SELECT * FROM zoo.animal');
-    //(err, results, fields) => {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-      res.json(results);
-    } catch (e){
-        console.log(e);
-        res.send('db error :(');
-
-    }
-    
+  try {
+    res.json(await animal.getAll());
+  } catch (e) {
+    //console.log(e);
+    //res.send('db error :(');
+  }
 });
 
 app.get('/animal', async (req, res) => {
-console.log(req);
-//res.send(`query params? ${req.query}`);
-try{
-    const[results] = await connection.query(
-        'SELECT * FROM zoo.animal WHERE name LIKE ?',
-        [req.query.name]);
-    
-    /*const [results] = await connection.query(
-        `SELECT * FROM animal WHERE name LIKE '${req.query.name}'`);
-    */
-        res.json(results);
-    }
-    catch(e){
-        res.send(`db error ${e}`);
-    }
-    
+  console.log(req.query);
+  try {
+    res.json(await animal.search(req.query.name));
+  } catch(e) {
+    res.send(`db error`);
+  }
+});
 
+app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) => {
+  console.log(req.body);
+  try {
+    res.json(await animal.insert(req.body.name));
+  } catch (e) {
+    console.log(e);
+    res.send('db error');
+  }
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello from my Node server');
+  res.send('Hello form my Node server');
 });
 
 app.get('/demo', (req, res) => {
-    console.log("demo app");
-    res.send('demo');
-    });
-
+  console.log('request', req);
+  res.send('demo');
+});
 
 app.listen(3000, () => {
-    console.log(`server app start? listening at por `);
+  console.log('server app start?');
 });
